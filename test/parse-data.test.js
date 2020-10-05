742fe09a125d57cdb9378bf2a7bc41e8ab276c95
tomjohnhall <thomasjohnhall@gmail.com>
2020-10-05 13:53:28 +0100

    parse top level string arrays in data

--- test/parse-data.test.js
+++ test/parse-data.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const makeHook = require('../lib/parse-data')
 
-const parseData = makeHook({ fields: ['title', 'description'] })
+const parseData = makeHook({ fields: ['title', 'description', 'tags'] })
 
 describe('Data', function () {
   it('transforms the data (object)', function (done) {
@@ -74,6 +74,29 @@ describe('Data', function () {
 
       assert(data[0]['description']['fr'] === 'a description', 'we have a modified the data to map the language')
       assert(data[1]['description']['en'] === 'another description', 'we have a modified the data to map the language')
+      done()
+    } catch (error) {
+      done(error)
+    }
+  })
+
+  it('transforms top level array', function (done) {
+    const context = {
+      type: 'before',
+      method: 'create',
+      data:
+        {
+          name: 'a name',
+          tags: ['tag1', 'tag2', 'tag3']
+        }
+
+    }
+    try {
+      parseData(context)
+      const { data } = context
+
+      assert(data['tags'][0]['en'] === 'tag1')
+      assert(data['tags'][2]['en'] === 'tag3', 'we have modified a top level array in data')
       done()
     } catch (error) {
       done(error)
